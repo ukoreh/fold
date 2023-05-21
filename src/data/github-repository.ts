3a571304877540dbdb60a01e006a0173e45a1319
RuteSantos <1160663@isep.ipp.pt>
2023-05-21 12:16:01 +0100

    fix: validation of github repository url

--- src/data/github-repository.ts
+++ src/data/github-repository.ts
@@ -2,21 +2,23 @@ export class GitHubRepository {
     private constructor(private readonly url: string, public readonly owner: string, public readonly name: string) {
     }
 
-    static from(url: string | null): GitHubRepository | Error{
-        if(!url){
+    static from(urlString: string | null): GitHubRepository | Error{
+        if(!urlString){
             return new Error("URL must not be null");
         }
 
-        const urlParts = url.split('/');
+        const url = new URL(urlString);
 
-        if(urlParts.length < 4) {
+        const pathParts = url.pathname.split('/').filter((value) => value && value.trim() !== "");
+
+        if(pathParts.length == 0) {
             return new Error("URL must contain the owner and repository name");
         }
         
-        if(urlParts.length < 5) {
+        if(pathParts.length <= 1) {
             return new Error("URL must contain the repository name");
         }
 
-        return new GitHubRepository(url, urlParts[3], urlParts[4])
+        return new GitHubRepository(urlString, pathParts[0], pathParts[1]);
     }
 }
