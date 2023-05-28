53d8255db996a31020eede49c6503e54390584bb
freitzzz <1160907@isep.ipp.pt>
2023-05-28 11:42:26 +0100

    feat: integrate download job logs request on handler

--- src/http/handler.ts
+++ src/http/handler.ts
@@ -3,29 +3,47 @@ import guid from "../data/guid";
 import { ForkCloneBuildDeployAction } from "../data";
 import { requestToGitHubRepository } from "../data";
 import { BadRequest, WorkflowInit, InternalServerError, corsResponse } from "./response";
+import { extractJobId, hasDownloadLogsHeaderSet } from "./header";
 
 export default async function (req: Request, config: Config): Promise<Response> {
     if (req.method === 'OPTIONS') {
         return Promise.resolve(corsResponse(req));
     }
 
-    const githubRepo = requestToGitHubRepository(req);
-    
-    if(githubRepo instanceof Error){
-        return Promise.resolve(new BadRequest(githubRepo.message).toResponse());
+    const jobStepRunId = guid();
+
+    const action = new ForkCloneBuildDeployAction(config.githubToken, config.forkOrg, config.actionRepoOwner, config.actionRepoName, config.actionWorkflowId, config.actionRefBranch);
+
+    if (hasDownloadLogsHeaderSet(req.headers)) {
+        const jobId = Number.parseInt(extractJobId(req.headers) ?? '0');
+
+        const logs = await action.getWorkflowRunLogs(config.actionRepoOwner, config.actionRepoName, jobId);
+
+        if (logs instanceof Error) {
+            return Promise.resolve(new BadRequest(logs.message).toResponse());
+        }
+
+        return Promise.resolve(new Response(logs, {
+            headers: {
+                'access-control-expose-headers': '*',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }));
     }
 
-    const jobStepRunId = guid();
-    
-    const action = new ForkCloneBuildDeployAction(config.githubToken, config.forkOrg, config.actionRepoOwner, config.actionRepoName, config.actionWorkflowId, config.actionRefBranch);
+    const githubRepo = requestToGitHubRepository(req);
+
+    if (githubRepo instanceof Error) {
+        return Promise.resolve(new BadRequest(githubRepo.message).toResponse());
+    }
 
     action.createWorkflowDispatch(githubRepo.owner, githubRepo.name, jobStepRunId);
 
     await sleep(15000);
 
     const workflow = await action.getWorkflowRun(jobStepRunId);
-    
-    if(workflow instanceof Error){
+
+    if (workflow instanceof Error) {
         return Promise.resolve(new InternalServerError(workflow.message).toResponse());
     }
 
