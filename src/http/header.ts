39374e286f60b63348e2f2d2dacd626daa415184
freitzzz <1160907@isep.ipp.pt>
2023-05-28 11:39:50 +0100

    feat: add headers sent for download logs request

--- src/http/header.ts
+++ src/http/header.ts
@@ -1,5 +1,15 @@
 const repoUrl = 'x-repo-url'
+const jobId = 'x-job-id'
+const downloadLogs = 'x-download-logs'
 
 export function extractRepoUrl(headers: Headers): string | null {
     return headers.get(repoUrl);
+}
+
+export function extractJobId(headers: Headers): string | null {
+    return headers.get(jobId);
+}
+
+export function hasDownloadLogsHeaderSet(headers: Headers): boolean {
+    return headers.has(downloadLogs);
 }
